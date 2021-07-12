07c1f5a9ec303877cb01a09480736afa4897b85c
mush1200 <mush120@hotmail.com>
2021-07-12 22:21:29 +0800

    feat: add checked useremail&password function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exhbs = require('express-handlebars')
 const app = express()
 const port = 3000
+//載入資料庫User model
+const User = require('./models/user')
+const bodyParser = require('body-parser')
 
 //載入mongoose
 const mongoose = require('mongoose')
@@ -21,9 +24,25 @@ db.once('open', () => {
 app.engine('handlebars',  exhbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true} ))
 //設置路由
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  const msg = 'Username 或Password 錯誤'
+  User.find({ email: req.body.email, password: req.body.password
+  })
+    .lean()
+    .then(function (user) {
+      if (user.length === 1) {
+        res.render('welcome', {firstName: user[0].firstName})
+      } else {
+        res.render('index', {email: req.body.email, password: req.body.password, msg: msg})
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 //監聽器
