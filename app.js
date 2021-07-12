9e8f759e3a680e95a9a6771ebca34a3150a48528
mush1200 <mush120@hotmail.com>
2021-07-12 18:11:08 +0800

    feat:add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const exhbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+//載入mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/user-data', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+//連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 //載入模板引擎
 app.engine('handlebars',  exhbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
